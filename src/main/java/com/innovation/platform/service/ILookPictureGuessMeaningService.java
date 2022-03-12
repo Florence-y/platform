5509e7768d58aaf47e84ee292f981bd40a8c2531
florenwu <florenwu@tencent.com>
2022-03-12 15:38:10 +0800

    Service

---
+++ src/main/java/com/innovation/platform/service/ILookPictureGuessMeaningService.java
@@ -0,0 +1,16 @@
+package com.innovation.platform.service;
+
+import com.innovation.platform.po.LookPictureGuessMeaning;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author Florence
+ * @since 2022-03-12
+ */
+public interface ILookPictureGuessMeaningService extends IService<LookPictureGuessMeaning> {
+
+}
---
+++ src/main/java/com/innovation/platform/service/IUserWordExplainService.java
@@ -0,0 +1,16 @@
+package com.innovation.platform.service;
+
+import com.innovation.platform.po.UserWordExplain;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author Florence
+ * @since 2022-03-12
+ */
+public interface IUserWordExplainService extends IService<UserWordExplain> {
+
+}
