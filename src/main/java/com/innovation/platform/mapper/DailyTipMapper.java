07a55b91cfc74d0c905b83fb76aba7fa8e4b74d3
florenwu <florenwu@tencent.com>
2022-03-12 15:37:43 +0800

    dailyTip

---
+++ src/main/java/com/innovation/platform/mapper/DailyTipMapper.java
@@ -0,0 +1,16 @@
+package com.innovation.platform.mapper;
+
+import com.innovation.platform.po.DailyTip;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Florence
+ * @since 2022-03-12
+ */
+public interface DailyTipMapper extends BaseMapper<DailyTip> {
+
+}
---
+++ src/main/java/com/innovation/platform/service/impl/DailyTipServiceImpl.java
@@ -0,0 +1,20 @@
+package com.innovation.platform.service.impl;
+
+import com.innovation.platform.po.DailyTip;
+import com.innovation.platform.mapper.DailyTipMapper;
+import com.innovation.platform.service.IDailyTipService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author Florence
+ * @since 2022-03-12
+ */
+@Service
+public class DailyTipServiceImpl extends ServiceImpl<DailyTipMapper, DailyTip> implements IDailyTipService {
+
+}
