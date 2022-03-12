53d38cd922564f2c2ca94a19ddd59cafc67b0c3e
florenwu <florenwu@tencent.com>
2022-03-12 15:46:49 +0800

    add table hello

---
+++ src/main/java/com/innovation/platform/controller/HelloController.java
@@ -0,0 +1,10 @@
+package com.innovation.platform.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/platform/test")
+public class HelloController {
+
+
+}
