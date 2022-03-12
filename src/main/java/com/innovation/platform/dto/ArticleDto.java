79cb939018d9a005a44a42048107b2c5fe93bc82
florenwu <florenwu@tencent.com>
2022-03-12 15:41:19 +0800

    Dto

---
+++ src/main/java/com/innovation/platform/dto/ArticleDto.java
@@ -0,0 +1,50 @@
+package com.innovation.platform.dto;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author Florence
+ * @since 2022-03-12
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class ArticleDto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * id
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * id
+     */
+    private Long userId;
+
+    /**
+     * URL
+     */
+    private String pictureUrl;
+
+    private String topic;
+
+    private String question;
+
+    private String content;
+
+    private LocalDateTime publishTime;
+
+
+}
---
+++ src/main/java/com/innovation/platform/dto/WordDto.java
@@ -0,0 +1,40 @@
+package com.innovation.platform.dto;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author Florence
+ * @since 2022-03-12
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class WordDto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * id
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * id
+     */
+    private Long editUserId;
+
+    private String content;
+
+    private String explain;
+
+
+}
