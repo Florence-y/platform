5f8e47b69ab40bdc2e9d6d5b2f3b9d90c1b7e8d9
florenwu <florenwu@tencent.com>
2022-03-12 15:38:34 +0800

    LookPictureGuess

---
+++ src/main/java/com/innovation/platform/service/impl/LookPictureGuessMeaningServiceImpl.java
@@ -0,0 +1,20 @@
+package com.innovation.platform.service.impl;
+
+import com.innovation.platform.po.LookPictureGuessMeaning;
+import com.innovation.platform.mapper.LookPictureGuessMeaningMapper;
+import com.innovation.platform.service.ILookPictureGuessMeaningService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author Florence
+ * @since 2022-03-12
+ */
+@Service
+public class LookPictureGuessMeaningServiceImpl extends ServiceImpl<LookPictureGuessMeaningMapper, LookPictureGuessMeaning> implements ILookPictureGuessMeaningService {
+
+}
